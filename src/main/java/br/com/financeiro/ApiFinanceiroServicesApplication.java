7749ec4ec3431a452016b73258dbdfa166e97fc7
Wanderson Xesquevixos de Siqueira <wanderson.siqueira@mandae.com.br>
2022-09-07 16:18:00 -0300

    Projeto inicial com conexão ao banco de dados

---
+++ src/main/java/br/com/financeiro/ApiFinanceiroServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceiroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceiroServicesApplication.class, args);
+	}
+
+}
